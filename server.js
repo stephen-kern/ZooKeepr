ae8b01e5b6c8927950cce134c5e6f765a9c254fc
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-09-19 14:51:05 -0500

    404 if the ID does not exist

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 // include the animal data file
 const { animals } = require('./data/animals.json');
 
-
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -46,7 +45,12 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
-  }
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
 
 
 app.get('/api/animals', (req, res) => {
@@ -55,6 +59,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
